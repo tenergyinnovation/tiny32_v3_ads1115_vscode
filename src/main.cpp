67629b80835460f6910f205f49476734b1d0c600
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-07-12 13:51:04 +0700

    rev1.0
    initial project

---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+/***********************************************************************
+ * Project      :     tiny32_v3_ADS1115
+ * Description  :     test chip ADS1115 ADC16bit with tiny32_v3
+ * Hardware     :     tiny32_v3         
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     04/07/2022
+ * Revision     :     1.0
+ * Rev1.0       :     Origital
+ * website      :     http://www.tenergyinnovation.co.th
+ * Email        :     admin@innovation.co.th
+ * TEL          :     +66 82-380-3299
+ ***********************************************************************/
+#include <Arduino.h>
+#include <tiny32_v3.h>
+#include <SPI.h>
+#include <Adafruit_ADS1X15.h>
+
+
+/**************************************/
+/*          Firmware Version          */
+/**************************************/
+String version = "0.1";
+
+/**************************************/
+/*        define object variable      */
+/**************************************/
+tiny32_v3 mcu;
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+                       /* ADDR pin short to GND default address = 0x48*/ 
+
+/**************************************/
+/*            GPIO define             */
+/**************************************/
+
+
+/**************************************/
+/*       Constand define value        */
+/**************************************/
+
+/**************************************/
+/*       eeprom address define        */
+/**************************************/
+
+/**************************************/
+/*        define global variable      */
+/**************************************/
+
+/**************************************/
+/*           define function          */
+/**************************************/
+
+/***********************************************************************
+ * FUNCTION:    setup
+ * DESCRIPTION: setup process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void setup()
+{
+    Serial.begin(115200);
+    Serial.printf("**** tiny32_v3_ADS1115 testing ****\r\n");
+
+
+    Serial.println("Getting single-ended readings from AIN0..3");
+    Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+
+    // The ADC input range (or gain) can be changed via the following
+    // functions, but be careful never to exceed VDD +0.3V max, or to
+    // exceed the upper and lower limits if you adjust the input range!
+    // Setting these values incorrectly may destroy your ADC!
+    //                                                                ADS1015  ADS1115
+    //                                                                -------  -------
+    // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+    // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+    // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+    // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+    // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+    // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+    if (!ads.begin()) {
+        Serial.println("Failed to initialize ADS.");
+        mcu.buzzer_beep(4);
+        while (1);
+    }
+
+    mcu.buzzer_beep(2);
+    mcu.TickBlueLED(0.5);
+
+}
+
+ /***********************************************************************
+ * FUNCTION:    loop
+ * DESCRIPTION: loop process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void loop()
+{
+  int16_t adc0, adc1, adc2, adc3;
+  float volts0, volts1, volts2, volts3;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+
+  volts0 = ads.computeVolts(adc0);
+  volts1 = ads.computeVolts(adc1);
+  volts2 = ads.computeVolts(adc2);
+  volts3 = ads.computeVolts(adc3);
+
+  Serial.println("-----------------------------------------------------------");
+  Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
+  Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
+  Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
+  Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(volts3); Serial.println("V");
+
+  delay(1000);
+}
+
+
